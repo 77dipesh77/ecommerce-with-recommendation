1140b36bc0627de523d8518005fbfd49ed4b68f3
77dipesh77 <155010574+77dipesh77@users.noreply.github.com>
2026-05-01 20:25:03 +0530

    Initialize Next.js TypeScript project

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */  
+   images: {
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
+  },
+  serverExternalPackages: ['@prisma/client', 'bcryptjs'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
